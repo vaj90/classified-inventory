879e49d3b7fbf94713912b1b85bb57b8b6da9c77
vaj90 <allanjohn.valiente@gmail.com>
2021-09-24 00:35:52 -0400

    Update code

--- sql_tables.sql
+++ sql_tables.sql
@@ -3,22 +3,42 @@ CREATE TABLE category
 	id     	   	INT            			 AUTO_INCREMENT, 
 	title	   	VARCHAR(45)   			 NOT NULL, 
 	description	VARCHAR(225)   			 NOT NULL, 
-	status     	enum('SHOW','HIDE')      DEFAULT 'SHOW', 
+	status     	enum('SHOW','HIDE')      	 DEFAULT 'SHOW', 
 	CONSTRAINT category_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE items 
 (
-	id     			INT            			AUTO_INCREMENT, 
+	id     		INT            			AUTO_INCREMENT, 
 	title	    	VARCHAR(225)   			NOT NULL, 
-	description		TEXT   					NOT NULL, 
+	description	TEXT   				NOT NULL, 
 	price       	DECIMAL(8,2)   			NOT NULL, 
-	cat_id        	INT			   			NOT NULL, 
-	status        	enum('SHOW','HIDE')     DEFAULT 'SHOW', 
-	image			VARCHAR(225)     		NOT NULL, 
+	cat_id        	INT			   	NOT NULL, 
+	status        	enum('SHOW','HIDE')     	DEFAULT 'SHOW', 
+	image		VARCHAR(225)     		NOT NULL, 
 	CONSTRAINT items_pk PRIMARY KEY (id),
 	CONSTRAINT category_items_fk_items FOREIGN KEY (cat_id)
 		REFERENCES category(id)
+);
+
+CREATE TABLE members 
+(
+	id     		    INT            		AUTO_INCREMENT,
+	first_name	    VARCHAR(45)   		NOT NULL,
+	last_name	    VARCHAR(45)   		NOT NULL,
+	username	    VARCHAR(45)   		NOT NULL,
+	password	    VARCHAR(45)   		NOT NULL,
+	email		    VARCHAR(45)   		NOT NULL,
+	CONSTRAINT members_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE track 
+(
+	id     		INT            			AUTO_INCREMENT,
+	item_id	    	int   				NOT NULL,
+	date_view	date   				NOT NULL,
+	count	    	int   				NOT NULL,
+	CONSTRAINT track_pk PRIMARY KEY (id)
 );
 
 INSERT INTO category (`title`, `description`, `status`) values 
@@ -49,34 +69,8 @@ INSERT INTO items (`title`, `description`, `price`, `image`, `cat_id`, `status`)
 ("HP 15.6 Gaming Laptop - Black","3.0GHz AMD Ryzen 5 4600H processor and 8GB DDR4-3200 SDRAM can run high-end games and applications without slowing down",899,"assets/img/5fa0964d7a5d19.17002620.png",7,"SHOW"),
 ("Sony BRAVIA 65 4K UHD HDR OLED Android Smart TV","65 4K Ultra HD display with 3840 x 2160 native resolution lets you enjoy movies, TV shows, games, and sports in stunning clarity",2499,"assets/img/5fa098a914ca37.75268040.png",6,"SHOW");
 
-CREATE TABLE members 
-(
-	id     			INT            		AUTO_INCREMENT,
-	first_name	    VARCHAR(45)   		NOT NULL,
-	last_name	    VARCHAR(45)   		NOT NULL,
-	username	    VARCHAR(45)   		NOT NULL,
-	password	    VARCHAR(45)   		NOT NULL,
-	email		    VARCHAR(45)   		NOT NULL,
-	CONSTRAINT members_pk PRIMARY KEY (id)
-);
-
-
-CREATE TABLE track 
-(
-	id     			INT            		AUTO_INCREMENT,
-	item_id	    	int   				NOT NULL,
-	date_view	    date   				NOT NULL,
-	count	    	int   				NOT NULL,
-	CONSTRAINT track_pk PRIMARY KEY (id)
-);
-
-
 INSERT INTO members (`first_name`, `last_name`, `username`, `password`,`email`) values 
-("Allan John","Valiente","allanjohn90","p@ssw0rd","allanjohn.valiente@georgebrown.ca"),
-("Farah","Sheherin","sfarah","sfarah","farah.sheherin@georgebrown.ca");
-
-
-
+("Allan John","Valiente","allan______","********","allanjohn.valiente@georgebrown.ca");
 
 /*Sample Data*/
 INSERT INTO items (`title`, `description`, `price`, `image`, `cat_id`, `status`) values 
@@ -84,7 +78,6 @@ INSERT INTO items (`title`, `description`, `price`, `image`, `cat_id`, `status`)
 ("Sample2","Description2",900,"",1,"SHOW");
 
 DELETE FROM items WHERE cat_id=1;
-
 
 INSERT INTO items (`title`, `description`, `price`, `image`, `cat_id`, `status`) values
 ('iPod touch', 'iPod touch gives you a beautiful canvas for your messages, photos, videos and more. Everything is sharp, vivid and lifelike. All on a device that’s 6.1 mm thin and just 88 grams, so you can take it anywhere.', 249, 'assets/img/5fd1ce006f19f6.95540577.jpg', 5, 'SHOW')
